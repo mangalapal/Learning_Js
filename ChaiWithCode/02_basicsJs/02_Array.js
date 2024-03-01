fa4dfd562cd21bee99777631d4b8682cd64667b0
mangalapal <mangalapal.mp@gmail.com>
2024-03-01 21:44:18 +0530

    update array method

--- ChaiWithCode/02_basicsJs/02_Array.js
+++ ChaiWithCode/02_basicsJs/02_Array.js
@@ -30,4 +30,4 @@ console.log(real_another_array);/*[
   let score2 = 200
   let score3 = 300
 
-  console.log(Array.of(score1, score2, score3));
+  console.log(Array.of(score1, score2, score3));//return new array of an element;[ 100, 200, 300 ]
