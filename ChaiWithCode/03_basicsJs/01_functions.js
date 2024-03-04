577890f4d0b7724dbaa8ec83cc63399f84a52a1f
mangalapal <mangalapal.mp@gmail.com>
2024-03-04 19:32:34 +0530

    Learning function

--- ChaiWithCode/03_basicsJs/01_functions.js
+++ ChaiWithCode/03_basicsJs/01_functions.js
@@ -36,11 +36,39 @@
 // }
 // console.log(loginUserMessage("Mangala"));
 
-function loginUserMessage (userName){
-    if(!userName){
-        console.log("Please Enter the user name");
-        return
-    }
-    return `${userName}! you are loggedIn`
+// function loginUserMessage (userName){
+//     if(!userName){
+//         console.log("Please Enter the user name");
+//         return
+//     }
+//     return `${userName}! you are loggedIn`
+// }
+// console.log(loginUserMessage());
+
+ function calculateCartPrice (val1, val2, ...num1){        //spread operator (...)
+    return num1
+ }
+//  console.log(calculateCartPrice(200, 400, 500));//[200, 400, 500]
+//  console.log(calculateCartPrice(200, 400, 500, 2000));// 500, 2000
+
+
+const user = {
+    username : "mangla Pal",
+    price: 199
 }
-console.log(loginUserMessage());
+function handleObjct(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObjct(user);
+handleObjct({
+    username : "Anand Pal",
+    price : 599
+});
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 100, 600]));
