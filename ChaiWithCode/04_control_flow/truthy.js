428936e3c77a28f5cc103110e1cbc225257d2903
mangalapal <mangalapal.mp@gmail.com>
2024-03-07 23:15:19 +0530

    learn truthy and nullish operator

--- ChaiWithCode/04_control_flow/truthy.js
+++ ChaiWithCode/04_control_flow/truthy.js
@@ -28,3 +28,17 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
+// Nulish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5 ?? 10 // 5
+//val1 = null ?? 10 // 10
+//val1 = undefined ?? 11 // 11
+
+
+
+console.log(val1);
+
+//terniary Operator
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("less than 80"): console.log("greater than 80");
