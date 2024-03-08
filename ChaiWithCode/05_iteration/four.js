35bee5bc068b05c8629ab793dee40afcaf5a699a
mangalapal <mangalapal.mp@gmail.com>
2024-03-08 10:30:18 +0530

    learn for in loop

---
+++ ChaiWithCode/05_iteration/four.js
@@ -0,0 +1,15 @@
+// const myObject = {
+//     js: "JavaScript",
+//     cpp: 'C++',
+//     rb: 'Ruby',
+//     swift: 'swift by apple'
+// }
+// for (const key in myObject) {
+//        console.log(`${key} shortcut is for ${myObject[key]}`);    
+//     }
+
+const programing = ['js', 'java', 'py', 'ruby', 'cpp']
+for(const key in programing){
+    //console.log(key);// it gives keys
+    console.log(programing[key]);//it gives value
+}
