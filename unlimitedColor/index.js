b930964421b89339105bc146ece35f91831c32a4
mangalapal <mangalapal.mp@gmail.com>
2024-03-24 22:27:04 +0530

    bg

--- unlimitedColor/index.js
+++ unlimitedColor/index.js
@@ -1,32 +1,29 @@
 // generate random color
 
-const randomColor = function(){
-    const hex = "0123456789ABCDEF";
-    let color = "#";
-    for(let i = 0; i < 6; i++){
-    color += hex[Math.floor(Math.random() * 16)]
-    }
-    return color;
-}
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
 let intervalId;
-const startChangingColor = function(){
-    if(!intervalId){
-        intervalId = setInterval(changeBgcolor, 1000);
-    }
-    
-    function changeBgcolor (){
-        document.body.style.backgroundColor = randomColor();
-    }
-}
+const startChangingColor = function () {
+  if (!intervalId) {
+    intervalId = setInterval(changeBgcolor, 1000);
+  }
 
-const stopChangingColor = function(){
-    clearInterval(intervalId);
-    intervalId = null;
-}
+  function changeBgcolor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
 
+const stopChangingColor = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
 
-document.querySelector('#start')
-.addEventListener('click', startChangingColor);
+document.querySelector("#start").addEventListener("click", startChangingColor);
 
-document.querySelector('#stop')
-.addEventListener('click', stopChangingColor)
+document.querySelector("#stop").addEventListener("click", stopChangingColor);
